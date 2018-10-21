9ac4a274704e6b695648b8cbfdd24230ae72f04c
hackerspark <hackerspark@gmail.com>
2018-10-22 00:03:58 +0530

    Added events

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 const app = express();
-
+const server = http.createServer(app);
+const io = socketIO(server);
+io.on("connection", socket => {
+  console.log("Welcome onboard");
+  socket.on("disconnect", () => console.log("Bye!"));
+});
 app.use(express.static(publicPath));
-app.listen(port, () => console.log(`Server is up on port ${port}`));
+server.listen(port, () => console.log(`Server is up on port ${port}`));
