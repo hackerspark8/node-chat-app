6aa4dd9ed1fa544e191ea8b331b83ec98e292546
hackerspark <hackerspark@gmail.com>
2018-10-23 00:34:23 +0530

    Added messaging events

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,6 @@
 const socket = io();
 socket.on("connect", () => {
   console.log("Connected!");
-  socket.emit("createMessage", {
-    from: "Deepak",
-    text: "Hey!"
-  });
 });
 socket.on("newMessage", message => console.log(message));
 socket.on("disconnect", () => console.log("Disconnected!"));
--- server/server.js
+++ server/server.js
@@ -10,14 +10,14 @@ const server = http.createServer(app);
 const io = socketIO(server);
 io.on("connection", socket => {
   console.log("Welcome onboard");
-  socket.on("createMessage", message => console.log(message));
-  socket.on("disconnect", () => console.log("Bye!"));
-
-  socket.emit("newMessage", {
-    from: "Sowmya",
-    text: "Hi",
-    createdAt: new Date().getTime()
+  socket.on("createMessage", ({ from, text }) => {
+    io.emit("newMessage", {
+      from,
+      text,
+      createdAt: new Date().getTime()
+    });
   });
+  socket.on("disconnect", () => console.log("Bye!"));
 });
 app.use(express.static(publicPath));
 server.listen(port, () => console.log(`Server is up on port ${port}`));
